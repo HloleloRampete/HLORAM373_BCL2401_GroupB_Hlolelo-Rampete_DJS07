58e289a977f8165b376c511dc942d8df42e05c84
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-30 11:43:29 +0200

    "Added import statement for Header component and included it in App component's return statement."

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <nav className="header">
+            <img src="./images/troll-face.png" alt="App logo" className="header--image" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Raect Course - Project 3</h4>
+        </nav>
+    )
+}
