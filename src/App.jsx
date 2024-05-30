b19bc8101164cd3ad28a674e0ec5c4e57eb09651
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-30 21:48:46 +0200

    "Removed memesData.jsx file and updated import path in Meme.jsx; added semicolons to end of lines in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import Header from "./Header.jsx"
-import Meme from "./Meme.jsx"
+import Header from "./Header.jsx";
+import Meme from "./Meme.jsx";
 
-function App() {
-
+export default function App() {
   return (
     <>
       <Header />
       <Meme />
     </>
-  )
+  );
 }
 
-export default App
+
