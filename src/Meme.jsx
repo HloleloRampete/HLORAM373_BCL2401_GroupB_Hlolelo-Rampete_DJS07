af5559cf209bb832a9a1f0b6be7e3d1d9aec1259
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-30 22:02:52 +0200

    "Removed import of memesData, replaced with API fetch in useEffect to retrieve memes data"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import memesData from "./memesData.jsx";
+
 
 export default function Meme() {
   const [meme, setMeme] = React.useState({
@@ -8,7 +8,13 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemesImages, setAllMemesImages] = React.useState(memesData);
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then((res) => res.json())
+    .then((data) => setAllMemes(data.data.memes))
+  }, [])
 
   function getMemeImage() {
     const memesArray = allMemesImages.data.memes;
