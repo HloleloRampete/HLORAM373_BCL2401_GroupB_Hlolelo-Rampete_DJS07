a36869c86cb91f3d3daab6ea627b2c64c59928f0
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-30 21:57:45 +0200

    "Deleted memesData.jsx file and updated import path in Meme.jsx"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import memesData from "../memesData.jsx";
+import memesData from "./memesData.jsx";
 
 export default function Meme() {
   const [meme, setMeme] = React.useState({
