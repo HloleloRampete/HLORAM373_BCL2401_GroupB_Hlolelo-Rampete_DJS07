0eac311bfb873e0ff0ba447ed4ab337c7867bea6
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-30 13:45:47 +0200

    "Refactored Meme component to use labeled input fields and added placeholders"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,12 +1,28 @@
 export default function Meme() {
-    return (
-        // including meme with form to reduce complexity 
-        <main>
-            <form action="submit" className="form">
-                <input type="text" className="form--input" placeholder="top text" />
-                <input type="text" className="form--input" placeholder="bottom text" />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
-        </main>
-    )
+  return (
+    // including meme with form to reduce complexity
+    <main>
+      <form action="submit" className="form">
+        <div>
+          <label htmlFor="top-text">Top text</label>
+          <input
+            id="top-text"
+            type="text"
+            className="form--input"
+            placeholder="Me at night"
+          />
+        </div>
+        <div>
+          <label htmlFor="bottom-text">Bottom text</label>
+          <input
+            id="bottom-text"
+            type="text"
+            className="form--input"
+            placeholder="Me in the Morning"
+          />
+        </div>
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
 }
